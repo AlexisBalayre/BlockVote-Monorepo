3a1ea7cc45d96aeef7cf245ecc0da3855951a5cc
Alexis Balayre <60859013+AlexisBal@users.noreply.github.com>
2023-03-06 17:49:59 +0100

    Init hardhat project with semaphore

--- contracts/README.md
+++ contracts/README.md
@@ -1,13 +1,11 @@
-# Sample Hardhat Project
+# BlockVote Contracts 
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+## To Start
+    ```git clone git@github.com:AlexisBal/BlockVote-Monorepo.git````
+    
+    ```cd BlockVote-Monorepo```
 
-Try running some of the following tasks:
-
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.ts
-```
+    ```cd contracts```
+    
+    ```yarn install```
+    
--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,45 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { config as dotenvConfig } from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+import { NetworksUserConfig } from "hardhat/types";
+import "./tasks/deploy";
+
+dotenvConfig()
+
+function getNetworks(): NetworksUserConfig {
+    if (process.env.ETHEREUM_URL && process.env.ETHEREUM_PRIVATE_KEY) {
+        const accounts = [`0x${process.env.ETHEREUM_PRIVATE_KEY}`]
+
+        return {
+            goerli: {
+                url: process.env.ETHEREUM_URL,
+                chainId: 5,
+                accounts
+            },
+            arbitrum: {
+                url: process.env.ETHEREUM_URL,
+                chainId: 42161,
+                accounts
+            }
+        }
+    }
+
+    return {}
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
-};
+    solidity: "0.8.18",
+    networks: {
+        hardhat: {
+            chainId: 1337
+        },
+        ...getNetworks()
+    },
+    gasReporter: {
+        currency: "USD",
+        enabled: process.env.REPORT_GAS === "true",
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY
+    }
+}
 
-export default config;
+export default config
